bd2c72c98824a0b26b7ac0f866fc0ca38c9ae199
Sujalk63 <sujalkarmakar6363@gmail.com>
2025-05-02 17:30:35 +0530

    hunting user profile 70% completed

--- hunting_data/modules/hunt_user_profile_data.py
+++ hunting_data/modules/hunt_user_profile_data.py
@@ -3,6 +3,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from utilities.convert_to_number import convert_to_number
 import pandas as pd  # Importing pandas for saving to Excel
+import re
+
+t = 2
 
 def scrape_profiles(driver, usernames=None):
 
@@ -15,7 +18,7 @@ def scrape_profiles(driver, usernames=None):
     """
     if usernames is None:
         # Batch mode from Excel
-        df = pd.read_excel("usernames.xlsx")
+        df = pd.read_excel("usernames_dummy.xlsx")
         usernames_list = df["Username"].dropna().unique().tolist()
     elif isinstance(usernames, str):
         # Single username mode
@@ -55,8 +58,8 @@ def fetch_profile_data(driver, username):
         "Thread Link": None,
         "External Link": [],
         "Email in Bio": None,
-        "Collab/Mgmt Info": None,
-        "Business Category Label": None,
+        # "Collab/Mgmt Info": None,
+        # "Business Category Label": None,
         "Profile Picture URL": None,
         "Is Verified": False,
         "Professional Label": None
@@ -88,9 +91,21 @@ def fetch_profile_data(driver, username):
         # data["Thread Link"] = threadLink
         # print(f'{username}: {data["Thread Link"]}')
 
-        externalLink = external_link(driver)
-        data["External Link"] = externalLink
-        print(f'{username}: {data["External Link"]}')
+        # externalLink = external_link(driver)
+        # data["External Link"] = externalLink
+        # print(f'{username}: {data["External Link"]}')
+        
+        # email = extract_email_from_bio(profileBio)
+        # data["Email in Bio"] = email
+        # print(f'{username}: {data["Email in Bio"]}')
+
+        # ppLink = pp_link(driver, username)
+        # data["Profile Picture URL"] = ppLink
+        # print(f'{username}: {data["Profile Picture URL"]}')
+
+        isVerified = is_verified(driver)
+        data["Is Verified"] = isVerified
+        print(f'{username}: {data["Is Verified"]}')
         
 
     except Exception as e:
@@ -109,7 +124,7 @@ def get_full_name(driver):
     Returns the full name or None if not found.
     """
     try:
-        full_name_elem = WebDriverWait(driver, 10).until(
+        full_name_elem = WebDriverWait(driver, t).until(
             EC.presence_of_element_located((By.XPATH, '//header//section//div//span[contains(@class, "x1lliihq x1plvlek xryxfnj x1n2onr6 x1ji0vk5 x18bv5gf x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj")]'))
         )
         return full_name_elem.text.strip() if full_name_elem.text else None
@@ -121,7 +136,7 @@ def get_full_name(driver):
 # 2  
 def post_count(driver):
     try:
-        post_count_elem = WebDriverWait(driver, 10).until(
+        post_count_elem = WebDriverWait(driver, t).until(
             EC.presence_of_element_located((By.XPATH, '//ul//li[1]//span[contains(@class, "html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs")]'))
         )
         post_count_elem_text = post_count_elem.text.strip()
@@ -135,7 +150,7 @@ def post_count(driver):
 # 3
 def followers_count(driver):
     try:
-        followers_count_elem = WebDriverWait(driver, 10).until(
+        followers_count_elem = WebDriverWait(driver, t).until(
             EC.presence_of_element_located((By.XPATH, '//ul//li[2]//span[contains(@class, "html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs")]'))
         )
         followers_count_elem_text = followers_count_elem.text.strip()
@@ -148,7 +163,7 @@ def followers_count(driver):
 #  4
 def following_count(driver):
     try:
-        following_count_elem = WebDriverWait(driver, 10).until(
+        following_count_elem = WebDriverWait(driver, t).until(
             EC.presence_of_element_located((By.XPATH, '//ul//li[3]//span[contains(@class, "html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs")]'))
         )
         following_count_elem_text = following_count_elem.text.strip()
@@ -161,7 +176,7 @@ def following_count(driver):
 #5
 def profile_bio(driver):
     try:
-        bio_elem = WebDriverWait(driver, 10).until(
+        bio_elem = WebDriverWait(driver, t).until(
             EC.presence_of_element_located((By.XPATH, '//span//div//span[contains(@class, "_ap3a _aaco _aacu _aacx _aad7 _aade")]'))
         )
         bio_elem_text = bio_elem.text.strip()
@@ -174,7 +189,7 @@ def profile_bio(driver):
 #6
 def thread_link(driver):
     try:
-        thread_link_elem = WebDriverWait(driver, 10).until(
+        thread_link_elem = WebDriverWait(driver, t).until(
             EC.presence_of_element_located((By.XPATH, '//a//span[contains(@class, "x1lliihq x1plvlek xryxfnj x1n2onr6 x1ji0vk5 x18bv5gf x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye x1fhwpqd xo1l8bm x5n08af x1s3etm8 x676frb x10wh9bi x1wdrske x8viiok x18hxmgj")]'))
         )
         thread_link = thread_link_elem.text.strip()
@@ -188,7 +203,7 @@ def thread_link(driver):
 def external_link(driver): # when fetching external links the browser need to be opened
     try:
         # Try to find and click the link button
-        link_button = WebDriverWait(driver, 5).until(
+        link_button = WebDriverWait(driver, t).until(
             EC.element_to_be_clickable((By.XPATH, '//section//button[@class=" _acan _acao _acas _aj1- _ap30"]'))
         )
 
@@ -201,7 +216,7 @@ def external_link(driver): # when fetching external links the browser need to be
 
     try:
         # After clicking, wait for the popup and get all <a> links
-        popup_links = WebDriverWait(driver, 5).until(
+        popup_links = WebDriverWait(driver, t).until(
             EC.presence_of_all_elements_located((By.XPATH, '//div[contains(@role, "dialog")]//a'))
         )
         external_links = [link.get_attribute('href') for link in popup_links]
@@ -211,15 +226,49 @@ def external_link(driver): # when fetching external links the browser need to be
     except Exception:
         print("No links found in popup.")
         return []
-
-
-
-
-
     
-
-# <button class=" _acan _acao _acas _aj1- _ap30" type="button"><div class="x3nfvp2 x193iq5w"><span class="x1hwekxz xyqdw3p"><svg aria-label="Link icon" class="x1lliihq x1n2onr6 x7l2uk3" fill="currentColor" height="12" role="img" viewBox="0 0 24 24" width="12"><title>Link icon</title><path d="m9.726 5.123 1.228-1.228a6.47 6.47 0 0 1 9.15 9.152l-1.227 1.227m-4.603 4.603-1.228 1.228a6.47 6.47 0 0 1-9.15-9.152l1.227-1.227" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="8.471" x2="15.529" y1="15.529" y2="8.471"></line></svg></span><div class="_ap3a _aaco _aacw _aacz _aada _aade" dir="auto">www.youtube.com/@futuregenius1474 + 1</div></div></button>
-
+# 8
+def extract_email_from_bio(profileBio):
+    if not profileBio:
+        return None
+    
+    # Regex to match most email formats
+    match = re.search(r'[\w\.-]+@[\w\.-]+\.\w+', profileBio)
+    
+    if match:
+        return match.group(0)
+    else:
+        return None
+    
+# 9
+def pp_link(driver, username):
+    try:
+        pp_link_elem = WebDriverWait(driver, t).until(
+            EC.presence_of_element_located((By.XPATH, f'//section//span//img[contains(@alt, "{username}")]'))
+        )
+        profile_pic_url = pp_link_elem.get_attribute('src')
+        pp_link = profile_pic_url.strip()
+        value = pp_link
+        return value if value else None
+    
+    except Exception as e:
+        print(e)
+        return None
+    
+# 10
+def is_verified(driver):
+    try:
+        verified_elem = WebDriverWait(driver, t).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]/section/main/div/header/section[2]/div/div/div[1]/div/div/svg/path'))
+        )
+        if verified_elem:
+            return True
+        else:
+            return False
+    
+    except Exception as e:
+        # print(e)
+        return False
 
 
 
